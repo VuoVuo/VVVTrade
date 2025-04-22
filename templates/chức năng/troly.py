c7880c2d8bf3845ba79e9f8edc8acbea85371137
Rames II <fwvuong@gmail.com>
2025-04-22 15:13:42 +0700

    Update troly.py

--- templates/chức năng/troly.py
+++ templates/chức năng/troly.py
@@ -1,10 +1,6 @@
 import os
 import streamlit as st
 from langchain_community.document_loaders import PyPDFLoader
-
-loader = PyPDFLoader("duong_dan_toi_file.pdf")
-pages = loader.load_and_split()
-
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain.embeddings import OpenAIEmbeddings
 from langchain.vectorstores import Chroma
